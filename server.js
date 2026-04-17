7a3463d2942ea227e64d76cf93212e5bac56f855
ashrita543 <ashritavetcha@gmail.com>
2026-04-17 22:35:20 +0530

    Add root route

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Server is running 🚀');
+});
 
 const PORT = process.env.PORT || 3000;
 
